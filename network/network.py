832849e9f0e7e3359c5b79159b78ffce5a1b5641
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2024-10-17 23:41:57 -0400

    added environment variables

--- network/network.py
+++ network/network.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
 
 def send_email(message_details: dict):
     """
@@ -10,19 +12,12 @@ def send_email(message_details: dict):
     -----------
     message_details : dict
         A dictionary containing the following keys:
-        
-        - 'sender_email' (str): The sender's email address (must be a valid Gmail address).
-        - 'receiver_email' (str): The recipient's email address.
-        - 'password' (str): The Gmail app password or OAuth 2.0 token for authentication.
         - 'subject' (str): The subject of the email.
         - 'body' (str): The plain-text content of the email.
 
     Example:
     --------
     message_details = {
-        "sender_email": "your_email@gmail.com",
-        "receiver_email": "recipient_email@example.com",
-        "password": "your_app_password",
         "subject": "Test Email",
         "body": "This is a test email from Python!"
     }
@@ -40,11 +35,12 @@ def send_email(message_details: dict):
     - The 'password' should be the app password generated via your Google Account's security settings.
     """
 
+    load_dotenv()
     # Create the email
     message = MIMEMultipart()
-    message["From"] = message_details["sender_email"]
-    message["To"] = message_details["receiver_email"]
-    message["Subject"] = message_details["subject"]
+    message["From"] = os.getenv("sender_email")
+    message["To"] = os.getenv("receiver_email")
+    message["Subject"] = os.getenv("subject")
 
     # Attach the email body
     message.attach(MIMEText(message_details["body"], "plain"))
