096bf8fc7bfa8eedca13e43c2ff4e5008b086871
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2024-10-17 23:56:19 -0400

    added basic database functionality

---
+++ database/db.py
@@ -0,0 +1,23 @@
+import requests
+from datetime import date
+
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        public_ip = response.json()['ip']
+        return public_ip
+    except Exception as e:
+        return f"Error occurred: {e}"
+
+class Database:
+    video_count = 1
+    current_date = date.today()
+
+    def __init__(self):
+        self.directory = "../videos/"
+    
+    def create_video(self):
+        self.video = self.directory + str(date.today()) + str(Database.video_count)
+        video_count +=1
+        Database.current_date = date.today()
+
