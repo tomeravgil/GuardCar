dd2949e3953326b014615956a4e3222f1ef6ced2
jdechiro05 <jackdech05@gmail.com>
2025-10-24 16:54:09 -0400

    Created main to process mp4 files
    Created the main to process the mp4 files

--- yolo-detection/test.py
+++ yolo-detection/test.py
@@ -24,3 +24,43 @@ class VideoRunner:
             frame_scores.append(score)
 
         return frame_scores
+
+if __name__ == "__main__":
+    model = "yolov8n.pt"        
+    testVideofolder = "testVideos"    
+
+    if not os.path.isdir(testVideofolder):
+        raise FileNotFoundError(f"Folder '{TEST_VItestVideofolderDEO_DIR}' not found.")
+
+    videoFiles = [
+        os.path.join(testVideofolder, f)
+        for f in os.listdir(testVideofolder)
+        if f.lower().endswith(".mp4")
+    ]
+
+    if not videoFiles:
+        raise FileNotFoundError(f"No files found in '{TEST_VIDEO_DIR}'")
+
+    runner = VideoRunner(model)
+
+    # 4. loop over each video
+    for videoPath in videoFiles:
+        print(f"\n===== Processing video: {videoPath} =====")
+
+        frameScores = runner.run_on_video(videoPath)
+
+        for i, score in enumerate(frameScores):
+            print(f"Frame {i:04d} | Suspicion score: {score:.6f}")
+
+        if frameScores:
+            avgScore = sum(frameScores) / len(frameScores)
+            maxScore = max(frameScores)
+            minScore = min(frameScores)
+
+            print("----- Summary -----")
+            print(f"Frames processed: {len(frameScores)}")
+            print(f"Avg suspicion score: {avgScore:.6f}")
+            print(f"Max suspicion score: {maxScore:.6f}")
+            print(f"Min suspicion score: {minScore:.6f}")
+        else:
+            print("No frames processed")
