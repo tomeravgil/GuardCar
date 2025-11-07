6dba90e747c8c8b54b0cd1ebc8f1a6ca35a64b06
tomeravgil <tomeravgil@gmail.com>
2025-11-07 13:47:17 -0500

    working inference w/ cuda

--- detection/model/rf_detr/rf_detection.py
+++ detection/model/rf_detr/rf_detection.py
@@ -1,19 +1,18 @@
 from ..detection_service import DetectionService
 from inference.models.utils import get_model
 from typing import Optional
+import torch
 
 class RFDETRDetectionService(DetectionService):
     def __init__(self, model_path: str):
-        self.model = self.load_model(model_path)
+        self.model = get_model(
+            model_path,
+            providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
+        )
 
     def load_model(self, model_path: Optional[str] = None):
         """Load a RFDETR model from the specified path."""
-        try:
-            model = get_model(model_path, device="cuda")
-            return model
-        except Exception as e:
-            print(f"Error loading model: {e}")
-            return None
+        pass
         
     def detect(self, frame):
         """Perform object detection on an image."""
