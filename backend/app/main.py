aa58ebfa01bc39513d29aecbef9a622aeae1e5c7
jdechiro05 <jackdech05@gmail.com>
2025-12-11 18:52:16 -0500

    Get Current User and Videos dependency
    Get Current User  - This returns the bearer token if the token matches the one at the endpoint
    
    Now can only access Videos list if authenticated.

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.dependencies import init_dependencies
 from backend.app.core.services.minio.minio_service import init_minio_bucket
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Security
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from datetime import datetime
 from backend.app.api.routers import suspicion, video_stream
@@ -26,12 +26,7 @@ app.add_middleware(
     allow_methods=["*"],  # <-- ALLOW OPTIONS HERE
     allow_headers=["*"],
 )
-app.include_router(suspicion.router)
-app.include_router(sse.router)
-app.include_router(videos.router)
-app.include_router(cloud_config.router)
-app.include_router(suspicion_config.router)
-app.include_router(video_stream.router)
+
 
 shutdown_event = asyncio.Event()
 @app.on_event("startup")  
@@ -64,8 +59,30 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         )
 
     # if theres a correct login, return token for login, currently temp 
-    return {"access_token": "Access Token Return Success", "token_type": "Bearer"}
+    return {"access_token": "Access-Token-Success", "token_type": "Bearer"}
+
+# Extracts the bearer token from the authorization, 
+# then validates that the token matches the one at our login endpoint
+async def get_current_user(token: str = Security(oauth2_scheme)):
+    # Validates the bearer token and then validates the current fake token
+    if token != "Access-Token-Success":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # eventually return the real user information, but currently returning dummy user 
+    return {"user-name": "test"}
 
 @app.on_event("shutdown")
 async def on_shutdown():
     shutdown_event.set()
+
+# declare this after functions to be able to use get_current_user as a dependi
+app.include_router(suspicion.router)
+app.include_router(sse.router)
+# GET /api/videos requires current user token / bearer 
+app.include_router(videos.router, dependencies=[Depends(get_current_user)])
+app.include_router(cloud_config.router)
+app.include_router(suspicion_config.router)
+app.include_router(video_stream.router)
